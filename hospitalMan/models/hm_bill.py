8a953d8505b01ba07b64f6323b5b3d087b91391d
modm-odoo <modm@odoo.com>
2024-02-12 16:39:04 +0530

    relatedfield

--- hospitalMan/models/hm_bill.py
+++ hospitalMan/models/hm_bill.py
@@ -4,6 +4,6 @@ class Bill(models.Model):
     _name = "hospiman.bill"
 
     patient = fields.Many2one('hospiman.patient')
-    # casefrompatient = fields.Many2one('hospiman.patient', string="Case from Patient")
+    casefrompatient = fields.Integer(related="patient.caseno",readonly = True)
     billno = fields.Integer("Bill-No")
     Charges = fields.Integer("Total amount")
